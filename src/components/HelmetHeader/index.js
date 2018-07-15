fc86859c62d2674550573ca9a6b2fdb687f546ff
Eric Chaves <erchaves@gmail.com>
2018-07-14 20:57:51 -0400

    fixing reference to wrong react-router
    I’m not sure how it found the other one which is not in the
    package.json.

--- src/components/HelmetHeader/index.js
+++ src/components/HelmetHeader/index.js
@@ -1,5 +1,5 @@
 import React, { PropTypes, Component } from 'react';
-import { withRouter } from 'react-router';
+import { withRouter } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import { getMetaTags } from '../../helpers';
 
